71805adac3f5d5fe10ee2ad74f1b6bdb9dd16c08
artemkibitov <artemkibitov@gmail.com>
2022-12-21 14:53:15 +0200

    url function format

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import puppeteer from "puppeteer";
+import path from "node:path";
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const goPage = async () => {
@@ -6,6 +11,13 @@ const goPage = async () => {
 
     const page = await browser.newPage();
 
-    await page.goto('google.com');
-    
+    await page.goto('https://translate.google.com/');
+
+    const res = await page.waitForSelector('body');
+
+    console.log(res.$());
+    await browser.close();
 }
+
+console.log();
+goPage();
